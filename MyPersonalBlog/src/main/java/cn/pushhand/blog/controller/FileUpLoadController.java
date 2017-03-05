a3a6395899f8a23bd7eaa9f74cb33fbc585d2ccf
夏悸 <xiangzhiwu@outlook.com>
2017-03-05 16:01:30 +0800

    summernote上传图片功能改造（把图片传到目录保存，不存入数据库表中）

--- MyPersonalBlog/src/main/java/cn/pushhand/blog/controller/FileUpLoadController.java
+++ MyPersonalBlog/src/main/java/cn/pushhand/blog/controller/FileUpLoadController.java
@@ -1,17 +1,19 @@
 package cn.pushhand.blog.controller;
 
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import cn.pushhand.blog.util.FileUploadUtils;
 
 /**
  * 图片上传
@@ -20,27 +22,39 @@ import cn.pushhand.blog.util.FileUploadUtils;
  *
  */
 @Controller
-@RequestMapping("/admin")
 public class FileUpLoadController {
 	
-	@RequestMapping("/upload")
+	@RequestMapping(value="/admin/upload", method = RequestMethod.POST)
 	@ResponseBody
 	public String uploadFile(@RequestParam("file")MultipartFile multipartFile , 
-			HttpServletRequest request , HttpServletResponse response) throws Exception{
+			HttpServletRequest request, HttpServletResponse response) throws Exception{
 		
-		String filePath = null;
-		filePath = FileUploadUtils.upload(multipartFile , request);
+		String  fileName = multipartFile.getOriginalFilename();
+		//名称 
+		String fileBaseName = fileName.substring(0 , fileName.lastIndexOf("."));
+		//后缀
+		String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+		//获取当前系统时间戳，作为文件名的一部分
+		SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 		
+		//创建一个新的文件名称
+		String newFileName = sf.format(new Date());
+		newFileName = newFileName + "." + fileExt;
+		System.out.println();
+		//获取路径
+		String filePath = request.getSession().getServletContext().getRealPath("/") + "jsp\\img\\upload\\" + newFileName;
+		String  netfilePath = "http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath()+"/jsp/img/upload/"+newFileName;
 		
-		//如果filePath为空，则上传失败
-		if (filePath == null || filePath == ""){
-			return null;
+		//创建文件
+		File localFile = new File(filePath);
+		//不存在则自动创建
+		if(!localFile.exists()){
+			localFile.mkdir();
 		}
-		
-		ObjectMapper mapper = new ObjectMapper();
-		filePath = mapper.writeValueAsString(filePath);
-		return filePath;
-		
+		//存放
+		multipartFile.transferTo(localFile);
+		//System.out.println(filePath);
+		return netfilePath;		
 	}
 	
 }
